26c0de75dc0d5d32fc2319a8ffb302a2670ddd64
omardeiaa <112701247+omardeiaa@users.noreply.github.com>
2022-10-21 09:32:10 -0700

    Add files via upload

---
+++ network.py
@@ -0,0 +1,31 @@
+import socket
+import pickle
+
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "192.168.1.11"
+        self.port = 5555
+        self.addr = (self.server, self.port)
+
+    def p(self):
+        self.p = self.connect()
+
+    def getP(self):
+        return self.p
+
+    def connect(self):
+            try:
+                self.client.connect(self.addr)
+                return pickle.loads(self.client.recv(2048).decode())
+            except:
+                pass
+    
+    def send(self, data):
+        try:
+            self.client.send(pickle.dumps(data))
+            return pickle.loads(self.client.recv(2048).decode())
+        except socket.error as e:
+            str(e)
+
+
